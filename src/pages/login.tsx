9d097273a42e9345753908c846a5b073c91b8d1e
benhammoush <guillaumeammouche@gmail.com>
2022-11-02 14:41:20 +0100

    Login page bg color IV

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -22,7 +22,7 @@ const LoginPage = () => {
           )}
         >
           <div className="flex flex-col pt-12 w-full px-[120px] items-center">
-            <MedusaIcon />
+          <a className="uppercase text-xl-semi">0 | X</a>
             {resetPassword ? (
               <ResetTokenCard goBack={() => setResetPassword(false)} />
             ) : (
